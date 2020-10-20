93804c8e580187ece14fece934a7c7a2fcba3e2e
Michael.Erickson <ylnorthville@gmail.com>
2020-10-20 12:17:43 -0400

    Fixed CSS and added more CSS

--- Lake_House/settings.py
+++ Lake_House/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Lake_app',
+    'Lake_App',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
