03d975ef4647ce9a281da8d9e21ad31550d17cc5
Michael.Erickson <merickson51293@gmail.com>
2021-04-05 08:15:56 -0400

    added more to home page and changed the nav bar

--- Lake_App/urls.py
+++ Lake_App/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns=[
     path("", views.index),
+    path("bakeshophome", views.bakeshophome),
+    path("giftshophome", views.giftshophome),
+    path("printshophome", views.printshophome)
 ]
--- Lake_App/views.py
+++ Lake_App/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def bakeshophome(request):
+    return render(request, "bakeshophome.html")
+
+def giftshophome(request):
+    return render(request, "giftshophome.html")
+
+def printshophome(request):
+    return render(request, "printshophome.html")
